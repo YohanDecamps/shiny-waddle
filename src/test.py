ed6f48178d7e874599c7b41445f5965d37008f20
Eliot <eliot1.martin@epitech.eu>
2024-03-15 18:27:23 +0100

    [ADD] who is richest plagia

--- src/test.py
+++ src/test.py
@@ -1,34 +1,59 @@
-import json
 import random
+import csv
 
-data_name = input("give me the name of the data\n")
-
-with open(data_name) as f:
-   data_list = json.load(f)
+from config import Config
 
 data_dict = {}
-for item in data_list:
-   name = item["reg_name"]
-   data_dict[name] = item
 
-data_param = input("give me the field you wish to play\n")
-random_country_name, random_country_data = random.choice(list(data_dict.items()))
+def csv_to_dict(csv_file_path, key_column, value_column):
+   result_dict = {}
+   
+   with open(csv_file_path, 'r', newline='') as file:
+      csv_reader = csv.DictReader(file)
+      
+      for row in csv_reader:
+         key = row[key_column]
+         value = row[value_column]
+         
+         result_dict[key] = value
+
+   return result_dict
+
+for data in Config.DATAS:
+   data_dict[data] = csv_to_dict("Datasets/" + data + ".csv", "country", "score")
+
 
 found = False
+random_country_name1, random_country_data1 = random.choice(list(data_dict["Corruption"].items()))
+random_country_name2, random_country_data2 = random.choice(list(data_dict["Corruption"].items()))
+
+i = 0
 while not found:
-   country = input("gimme a country\n")
+   print("1 -", random_country_name1, "has more", "Corruption", "than", random_country_name2)
+   print("2 -", random_country_name1, "has less", "Corruption", "than", random_country_name2)
+
+   choice = 0
 
    try:
-      data_dict[country]
-   except KeyError:
-      print(country, "not found")
+      choice = int(input())
+   except ValueError:
       continue
 
-   if data_dict[country][data_param] > random_country_data[data_param]:
-      print("Too much", data_param, random_country_name)
-   elif data_dict[country][data_param] < random_country_data[data_param]:
-      print("Too few", data_param, random_country_name)
-   else:
-      found = True
+   if choice != 1 and choice != 2:
+      continue
 
-print("GG well played")
+   if choice == 1 and random_country_data1 < random_country_data2:
+       print("Youre right!")
+   elif choice == 2 and random_country_data1 > random_country_data2:
+       print("Youre right!")
+   elif random_country_data2 == random_country_data1:
+       print("Both are equals!")
+   else:
+       print("Rip bozo youre wrong!, Youre streak is:", i)
+       exit() 
+   
+   random_country_name1 = random_country_name2
+   random_country_data1 = random_country_data2
+
+   random_country_name2, random_country_data2 = random.choice(list(data_dict["Corruption"].items()))
+   i += 1
